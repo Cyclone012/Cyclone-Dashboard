6b8ef32499135a015070b065dc69d8f5f98b33db
Cyclone012 <129366524+Cyclone012@users.noreply.github.com>
2024-02-12 01:13:27 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const checkbox = document.getElementById("checkbox");
 const sidebar = document.querySelector(".sidebar");
 const content = document.querySelector(".content");
+const labelDarkLight = document.querySelector(".dark-light");
 
 // Function to set the theme based on the checkbox state
 function setTheme() {
   if (checkbox.checked) {
     document.body.classList.add("light");
     localStorage.setItem("theme", "light");
+    labelDarkLight.innerHTML = "Light";
   } else {
     document.body.classList.remove("light");
     localStorage.setItem("theme", "dark");
+    labelDarkLight.innerHTML = "Dark";
   }
 }
 
@@ -45,3 +48,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // Load the theme when the DOM content is loaded
 document.addEventListener("DOMContentLoaded", loadTheme);
+
+// toggle hamburger button
+document.addEventListener("DOMContentLoaded", function () {
+  var navIcon = document.querySelector(".hambuger-menu");
+  navIcon.addEventListener("click", function () {
+    this.classList.toggle("open");
+  });
+});
+
+// Mobile hover handle
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav_link");
+
+  // Function to toggle popup on touch events
+  function togglePopup(event) {
+    const popup = this.querySelector(".popup-text");
+    if (popup) {
+      popup.classList.toggle("show");
+      event.preventDefault(); // Prevent link navigation
+    }
+  }
+
+  // Add touch event listener to each navigation link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", togglePopup); // For tap event
+    link.addEventListener("touchstart", togglePopup); // For long press event
+  });
+});
